a8439f82e834de8f0a815f9f39c48c04fad2fd6b
DZariusz <dzariusz@gmail.com>
2021-02-07 17:08:26 +0100

    add: cancel, change address, remove bulk

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 import "./interfaces/Owned.sol";
+import "./UMB.sol";
 
 /// @title   Umbrella Rewards contract
 /// @author  umb.network
@@ -25,6 +26,8 @@ contract Rewards is Owned {
     ERC20 public rewardToken;
 
     uint256 public distributionStartTime;
+    uint256 public totalLinearRewards;
+
     mapping(address => Reward) public rewards;
     address[] public participants;
 
@@ -32,8 +35,9 @@ contract Rewards is Owned {
         uint256 total;
         uint256 duration;
         uint256 paid;
-        uint8 bulk; //
     }
+
+    bool public setupDone = false;
 
     // ========== CONSTRUCTOR ========== //
 
@@ -47,84 +51,106 @@ contract Rewards is Owned {
     }
 
     function balanceOf(address _address) public view returns (uint256) {
-        uint256 start = distributionStartTime;
+        uint256 startTime = distributionStartTime;
 
-        if (block.timestamp <= start) {
+        if (startTime == 0) {
             return 0;
         }
 
         Reward memory reward = rewards[_address];
 
-        if (block.timestamp >= start.add(reward.duration)) {
+        if (block.timestamp >= startTime.add(reward.duration)) {
             return reward.total.sub(reward.paid);
         }
 
-        return reward.bulk == 0
-            ? reward.total.mul(block.timestamp.sub(start)).div(reward.duration).sub(reward.paid)
-            : bulkProgress(reward.total, reward.duration, reward.bulk);
+        return _totalBalanceOf(reward, startTime).sub(reward.paid);
+    }
+
+    function _totalBalanceOf(Reward memory _reward, uint256 _startTime) internal view returns (uint256) {
+        return _reward.total.mul(block.timestamp.sub(_startTime)).div(_reward.duration);
     }
 
     // ========== MUTATIVE FUNCTIONS ========== //
 
     function claim() external {
-        uint256 balance = balanceOf(_msgSender());
+        uint256 balance = balanceOf(msg.sender);
         require(balance != 0, "you have no tokens to claim");
 
-        rewards[_msgSender()].paid = rewards[_msgSender()].paid.add(balance);
-        rewardToken.safeTransfer(_msgSender(), balance);
+        rewards[msg.sender].paid = rewards[msg.sender].paid.add(balance);
+        rewardToken.safeTransfer(msg.sender, balance);
 
-        emit LogClaimed(_msgSender(), balance);
+        emit LogClaimed(msg.sender, balance);
     }
 
     // ========== RESTRICTED FUNCTIONS ========== //
 
-    function startDistribution(
+    function start() external onlyOwner {
+        require(setupDone, "contract not setup");
+        require(rewardToken.balanceOf(address(this)) >= totalLinearRewards, "not enough tokens for rewards");
+
+        distributionStartTime = block.timestamp;
+    }
+
+    function cancel(address _participant) external onlyOwner {
+        uint256 startTime = distributionStartTime;
+        Reward memory reward = rewards[_participant];
+
+        uint256 rewardsSoFar = _totalBalanceOf(reward, startTime);
+        uint256 remain = reward.total.sub(rewardsSoFar);
+
+        UMB(address(rewardToken)).burn(remain);
+        rewards[_participant].total = rewardsSoFar;
+        rewards[_participant].duration = block.timestamp - startTime;
+        totalLinearRewards -= remain;
+
+        require(rewardToken.balanceOf(address(this)) >= totalLinearRewards, "not enough tokens for rewards");
+
+        emit LogCancelDistribution(_participant);
+    }
+
+    function changeAddress(address _participant, address _new) external onlyOwner {
+        require(rewards[_participant].total > 0, "_participant does not exists");
+        require(rewards[_new].total == 0, "address already in use");
+
+        rewards[_new] = rewards[_participant];
+        delete rewards[_participant];
+
+        emit LogChangeAddress(_participant, _new);
+    }
+
+    function setupDistribution(
         ERC20 _rewardToken,
-        uint256 _startTime,
         address[] calldata _participants,
-        uint256[] calldata _rewards,
-        uint256[] calldata _durations,
-        uint8[] calldata _bulks
+        uint256[] calldata _totalRewards,
+        uint256[] calldata _durations
     )
     external onlyOwner {
+        require(!setupDone, "LinearDistribution already setup");
         require(_participants.length != 0, "there is no _participants");
-        require(_participants.length == _rewards.length, "_participants count must match _rewards count");
+        require(_participants.length == _totalRewards.length, "_participants count must match _rewards count");
         require(_participants.length == _durations.length, "_participants count must match _durations count");
-        require(_participants.length == _bulks.length, "_participants count must match _bulks count");
-        require(_startTime != 0, "start time is empty");
+        require(address(_rewardToken) != address(0x0), "empty _rewardToken");
 
-        distributionStartTime = _startTime;
+        setupDone = true;
         uint256 sum = 0;
 
         for (uint256 i = 0; i < _participants.length; i++) {
-            require(_bulks[i] <= 100, "bulk must be between 0 - 100");
-            rewards[_participants[i]] = Reward(_rewards[i], _durations[i], 0, _bulks[i]);
-            sum = sum.add(_rewards[i]);
+            require(_participants[i] != address(0x0), "empty participant");
+            rewards[_participants[i]] = Reward(_totalRewards[i], _durations[i], 0);
+            sum = sum.add(_totalRewards[i]);
         }
-
-        require(_rewardToken.balanceOf(address(this)) >= sum, "not enough tokens for rewards");
 
         rewardToken = _rewardToken;
         participants = _participants;
+        totalLinearRewards = sum;
 
-        renounceOwnership();
-        emit LogBurnKey();
-    }
-
-    // ========== PRIVATE  ========== //
-
-    // bulk distribution is release % of total amount on particular day
-    // this day is also calculated by % eg:
-    // if we have 10 days and bulk = 20, then release will be
-    // 20% on day 2, 40% on day 4, 60% on day 6 etc
-    function bulkProgress(uint256 _totalAmount, uint256 _duration, uint8 _bulk) internal view returns (uint256) {
-        uint bulks = block.timestamp.sub(distributionStartTime).mul(100).div(_duration * _bulk);
-        uint256 result = _totalAmount.mul(_bulk).mul(bulks) / 100;
-        return result > _totalAmount ? _totalAmount : result;
+        emit LogSetup(sum);
     }
 
     // ========== EVENTS ========== //
 
+    event LogSetup(uint256 amount);
+    event LogChangeAddress(address indexed from, address indexed to);
+    event LogCancelDistribution(address indexed participant);
     event LogClaimed(address indexed recipient, uint256 amount);
-    event LogBurnKey();
 }
