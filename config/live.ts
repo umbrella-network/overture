bcf27db908668cca5b8b066983e91854929af759
DZariusz <dzariusz@gmail.com>
2021-02-08 15:15:16 +0100

    live config

--- config/live.ts
+++ config/live.ts
@@ -7,7 +7,9 @@ export const liveConfig: ConfigType = {
   contractRegistry: {},
   multiSig: {
     owners: [
-      {address: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7', power: 1}
+      {address: '0x8558fCFE23E53A9555D8aBf5544c9EFc1F108f3e', power: 2},
+      {address: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7', power: 1},
+      {address: 'alex', power: 1}
       ],
     requiredPower: 3
   },
@@ -16,7 +18,7 @@ export const liveConfig: ConfigType = {
     name: 'Umbrella',
     symbol: 'UMB',
     initialBalance: humanNumberToWei('5,000,000'),
-    initialHolder: '0x3aD81d17D3B5FF75558271E0264085a1E6935872' // AMM
+    initialHolder: '0x3aD81d17D3B5FF75558271E0264085a1E6935872' // AMM ????
   },
   auction: {
     address: ethers.constants.AddressZero,
@@ -28,10 +30,10 @@ export const liveConfig: ConfigType = {
       initialHolder: ethers.constants.AddressZero,
       rewardId: 1,
       swapDuration: 60 * 60 * 24 * 365 / 2,
-      maxAllowedTotalSupply: humanNumberToWei('40,833,334'),
+      maxAllowedTotalSupply: humanNumberToWei('40,833,334'), // maybe allow more?
     },
     farming: {
-      rewardsDistribution: ethers.constants.AddressZero,
+      rewardsDistribution: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7',
       tokenAmountForDeFiRewards: humanNumberToWei('40,833,333')
     },
   }
