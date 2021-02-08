8c854f2a59205588701cf88ce2102d748335bc88
DZariusz <dzariusz@gmail.com>
2021-02-08 15:59:20 +0100

    life config

--- config/live.ts
+++ config/live.ts
@@ -8,9 +8,9 @@ export const liveConfig: ConfigType = {
   multiSig: {
     owners: [
       {address: '0x8558fCFE23E53A9555D8aBf5544c9EFc1F108f3e', power: 2},
-      {address: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7', power: 1},
+      {address: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7', power: 1}, // deployer
       {address: '0x52aE9A8B9A962082B92AFa3B25a7D8Ba4392a151', power: 1},
-      {address: 'alex', power: 1}
+      {address: '0x24582593EdF3e9C17F34e3D02DD1D6Cf51800dDf', power: 1}
       ],
     requiredPower: 3
   },
@@ -19,7 +19,7 @@ export const liveConfig: ConfigType = {
     name: 'Umbrella',
     symbol: 'UMB',
     initialBalance: humanNumberToWei('5,000,000'),
-    initialHolder: '0x3aD81d17D3B5FF75558271E0264085a1E6935872' // AMM ????
+    initialHolder: '0x3601EC1e652a4Cc624c8C6FA74b00BdDaf5989fD' // AMM ????
   },
   stage1: {
     rUmb1: {
@@ -27,7 +27,8 @@ export const liveConfig: ConfigType = {
       initialHolder: ethers.constants.AddressZero,
       rewardId: 1,
       swapDuration: 60 * 60 * 24 * 365 / 2,
-      maxAllowedTotalSupply: humanNumberToWei('40,833,334'), // maybe allow more?
+      // original 40,833,334, we added more to allow increase in future:
+      maxAllowedTotalSupply: humanNumberToWei('80,833,334'),
     },
     farming: {
       rewardsDistribution: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7',
