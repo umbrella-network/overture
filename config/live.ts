60e3b4d520a6a136dc78a3a4c8e34ade2d2ca6db
DZariusz <dzariusz@gmail.com>
2021-02-08 16:14:36 +0100

    deployment addresses

--- config/live.ts
+++ config/live.ts
@@ -6,6 +6,7 @@ export const liveConfig: ConfigType = {
   env: 'live',
   contractRegistry: {},
   multiSig: {
+    address: '0x4A28406ECE8fFd7A91789738a5ac15DAc44bFa1b',
     owners: [
       {address: '0x8558fCFE23E53A9555D8aBf5544c9EFc1F108f3e', power: 2},
       {address: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7', power: 1}, // deployer
@@ -15,6 +16,7 @@ export const liveConfig: ConfigType = {
     requiredPower: 3
   },
   UMB: {
+    address: '0x6fC13EACE26590B80cCCAB1ba5d51890577D83B2',
     maxAllowedTotalSupply: humanNumberToWei('500,000,000'),
     name: 'Umbrella',
     symbol: 'UMB',
@@ -23,6 +25,7 @@ export const liveConfig: ConfigType = {
   },
   stage1: {
     rUmb1: {
+      address: '0x1B17DBB40fbED8735E7fE8C9eB02C20984fAdfD6',
       initialBalance: '0',
       initialHolder: ethers.constants.AddressZero,
       rewardId: 1,
@@ -31,6 +34,7 @@ export const liveConfig: ConfigType = {
       maxAllowedTotalSupply: humanNumberToWei('80,833,334'),
     },
     farming: {
+      address: '0xDa9A63D77406faa09d265413F4E128B54b5057e0',
       rewardsDistribution: '0xA6e4fFa19B213AbeA258ae72e8e1a209B9E543e7',
       tokenAmountForDeFiRewards: humanNumberToWei('40,833,333')
     },
